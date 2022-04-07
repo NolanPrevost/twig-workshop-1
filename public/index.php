78e688b17c2cd52740b316fc876e1def76cbc1a4
Nolan Prévost <nolan.prevost@gmail.com>
2022-04-07 10:51:02 +0200

    twig for life

--- public/index.php
+++ public/index.php
@@ -3,7 +3,17 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-
 $name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+
+$persons = [
+  ['name' => 'riri', 'age' => 10],
+  ['name' => 'fifi', 'age' => 20],
+  ['name' => 'loulou', 'age' => 30],
+]; 
+
+echo $twig->render('hello.html.twig', ['name' => $name]);
+
+echo $twig->render('person.html.twig', [ 'persons' => $persons]);
+
+
